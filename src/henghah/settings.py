810597ba2b7ef5178823f53868d11ac190b3fa2b
soasme <soasme@gmail.com>
2017-03-04 23:52:27 +1300

    add api stub.

--- src/henghah/settings.py
+++ src/henghah/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'henghah.language',
     'henghah.album',
+    'henghah.api',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from henghah.settings_local import *
+except ImportError:
+    pass

3f19a72ab6dafbc2a69d7d60a5f4a2a4d9645bb7
soasme <soasme@gmail.com>
2017-03-05 11:00:13 +1300

    Define format_clock_indication.

--- src/henghah/album/utils.py
+++ src/henghah/album/utils.py
@@ -14,3 +14,24 @@ def upload_to(directory, filename):
     now = datetime.utcnow()
     return '%s/%04d/%02d/%02d/%s' % (directory, now.year,
             now.month, now.day, resource_filename)
+
+# Progress
+
+def format_clock_indication(epoch):
+    """Transform an integer number into a clock indication string.
+
+    >>> format_progress(0)
+    "0:00:00.00"
+    >>> format_progress(5)
+    "0:00:05.00"
+    >>> format_progress(75)
+    "0:01:05.00"
+    """
+    millisecond = epoch % 100
+    epoch = epoch / 100
+    second = epoch % 60
+    epoch = epoch / 60
+    minute = epoch % 60
+    epoch = epoch / 60
+    hour = epoch % 24
+    return '%d:%02d:%02d.%02d' % (hour, minute, second, millisecond)

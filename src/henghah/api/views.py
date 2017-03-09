02726ce048425022c69fdc8780fcf9f625a2352d
soasme <soasme@gmail.com>
2017-03-09 13:43:44 +1300

    paginate api.

--- src/henghah/api/views.py
+++ src/henghah/api/views.py
@@ -9,23 +9,32 @@ from henghah.album.models import Series, Audio, Transcript
 
 class SeriesSerializer(serializers.HyperlinkedModelSerializer):
 
-    audios = serializers.HyperlinkedIdentityField('series-audios')
+    series_api = serializers.HyperlinkedIdentityField('series-detail')
+    audios_api = serializers.HyperlinkedIdentityField('series-audios')
 
     class Meta:
         model = Series
-        fields = ('slug', 'title', 'cover', 'description', 'created_at', 'updated_at', 'audios', )
+        fields = ('slug', 'title', 'cover', 'description', 'created_at', 'updated_at', 'audios_api', 'series_api')
 
 class SeriesViewSet(viewsets.ModelViewSet):
 
     queryset = Series.objects.all()
     serializer_class = SeriesSerializer
 
+    def get_queryset(self):
+        queryset = Series.objects.all()
+        slug = self.request.query_params.get('slug')
+        if slug:
+            queryset = queryset.filter(slug=slug)
+        return queryset
+
     @detail_route(methods=['GET'])
     def audios(self, request, pk):
         series = get_object_or_404(self.queryset, pk=pk)
         series_audios = Audio.objects.filter(series=series)
-        serializer = AudioSerializer(series_audios, many=True, context={'request': request})
-        return Response(serializer.data)
+        page = self.paginate_queryset(series_audios)
+        serializer = TrimAudioSerializer(page, many=True, context={'request': request})
+        return self.get_paginated_response(serializer.data)
 
 
 class TranscriptSerializer(serializers.ModelSerializer):
@@ -35,8 +44,20 @@ class TranscriptSerializer(serializers.ModelSerializer):
         fields = ('start', 'end', 'content', 'created_at', 'updated_at', )
 
 
+class TrimAudioSerializer(serializers.HyperlinkedModelSerializer):
+
+    audio_api = serializers.HyperlinkedIdentityField('audio-detail')
+
+    class Meta:
+        model = Audio
+        fields = ('title', 'audio', 'created_at', 'updated_at', 'audio_api', )
+
+
+
 class AudioSerializer(serializers.HyperlinkedModelSerializer):
+
     transcripts = TranscriptSerializer(many=True, read_only=True)
+
     class Meta:
         model = Audio
         fields = ('title', 'audio', 'created_at', 'updated_at', 'series', 'transcripts', )
@@ -45,6 +66,7 @@ class AudioSerializer(serializers.HyperlinkedModelSerializer):
 class AudioViewSet(viewsets.ModelViewSet):
     queryset = Audio.objects.all()
     serializer_class = AudioSerializer
+
 
 
 
